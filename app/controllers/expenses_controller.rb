1f1a5bdb85692c954234e0e8d7c00e89ef17a4a5
Kaushal Karki <kaushalsinghkarki@gmail.com>
2024-09-02 18:12:51 +0530

    Update expense API

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,5 +1,5 @@
 class ExpensesController < ApplicationController
-  before_action :set_expense, only: [:destroy]
+  before_action :set_expense, only: [:destroy, :show, :update]
   
   def create
     splits_details = params[:transaction][:expense_split]
@@ -11,7 +11,31 @@ class ExpensesController < ApplicationController
         transaction.expense_splits.create(user_id: splits[:userId], user_amount: splits[:amount], paid: false, owes: true)
       end
     end
+  end
 
+  def show
+    @expense = Expense.includes(:expense_splits).find(params[:id])
+    render json: @expense, include: :expense_splits
+  end
+
+  def update
+    splits_details = params[:transaction][:expense_split]
+    
+    if @expense.update(transaction_params)
+      @expense.expense_splits.destroy_all
+      
+      splits_details.each do |splits|
+        if splits[:userId].to_i == params[:transaction][:payer]
+          @expense.expense_splits.create(user_id: splits[:userId], user_amount: splits[:amount], paid: true, owes: false)
+        else
+          @expense.expense_splits.create(user_id: splits[:userId], user_amount: splits[:amount], paid: false, owes: true)
+        end
+      end
+
+      render json: @expense, include: :expense_splits
+    else
+      render json: { error: "Failed to update expense" }, status: :unprocessable_entity
+    end
   end
 
   def destroy
